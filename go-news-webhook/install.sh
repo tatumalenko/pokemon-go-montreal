4ffb1ff0a4c7786ae11e0aaf3792d1f82d3e74ca
tatumalenko <tatum.alenko@gmail.com>
2017-12-21 12:01:15 -0500

    new commit

--- go-news-webhook/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo cp go-news-webhook.service /etc/systemd/system/
-sudo systemctl enable /etc/systemd/system/go-news-webhook.service
-sudo systemctl enable go-news-webhook.service
-sudo systemctl restart go-news-webhook.service
