a086e80a1a4c5ef36af13b056788c3eb1c0c5c42
Tatum Alenko <tatum.alenko@gmail.com>
2018-01-24 23:36:41 -0500

    Add test cases for `Utils` class methods, namely `parseWildPreferenceQueryFromArgs` method.

--- test/pikachu.test.js
+++ test/pikachu.test.js
@@ -1,65 +1,44 @@
 const { test } = require('ava');
-const { parseQueryFromArgs } = require('../src/bots/pikachu/commands/want');
+const Utils = require('../src/utils/Utils');
 
-// const createMockWildPref = (name, neighbourhoods, iv, level) => ({
-//     name,
-//     neighbourhoods,
-//     iv,
-//     level,
-// });
-
-// console.log(createMockWildPref('blissey', ['everywhere'], 0, 0));
-
-test('want.js:parseQueryFromArgs', (t) => {
+test('Utils.js:parseWildPreferenceQueryFromArgs', (t) => {
     const cases = [];
     cases.deepEqual = [
         {
             input: 'pidgey 100',
-            expected: [{
-                name: 'pidgey',
-                neighbourhoods: [''],
-                iv: 100,
-                level: 0,
-            }],
+            expected: {
+                pokemonNames: ['pidgey'],
+                neighbourhoodNames: [],
+                ivs: [100],
+                levels: [],
+            },
         },
         {
             input: 'weedle village',
-            expected: [{
-                name: 'weedle',
-                neighbourhoods: ['village'],
-                iv: 0,
-                level: 0,
-            }],
+            expected: {
+                pokemonNames: ['weedle'],
+                neighbourhoodNames: ['village'],
+                ivs: [],
+                levels: [],
+            },
         },
         {
             input: 'all village rosemont 0 0',
-            expected: [{
-                name: 'all',
-                neighbourhoods: ['rosemont', 'village'],
-                iv: 0,
-                level: 0,
-            }],
+            expected: {
+                pokemonNames: ['all'],
+                neighbourhoodNames: ['rosemont', 'village'],
+                ivs: [0],
+                levels: [0],
+            },
         },
         {
             input: 'blissey tyranitar weedle village rosemont iv0 level 0',
-            expected: [{
-                name: 'blissey',
-                neighbourhoods: ['rosemont', 'village'],
-                iv: 0,
-                level: 0,
+            expected: {
+                pokemonNames: ['blissey', 'tyranitar', 'weedle'],
+                neighbourhoodNames: ['rosemont', 'village'],
+                ivs: [0],
+                levels: [0],
             },
-            {
-                name: 'tyranitar',
-                neighbourhoods: ['rosemont', 'village'],
-                iv: 0,
-                level: 0,
-            },
-            {
-                name: 'weedle',
-                neighbourhoods: ['rosemont', 'village'],
-                iv: 0,
-                level: 0,
-            }],
         },
     ];
     cases.throws = [
@@ -78,18 +57,45 @@ test('want.js:parseQueryFromArgs', (t) => {
     ];
 
     cases.deepEqual.forEach((item) => {
-        t.deepEqual(parseQueryFromArgs(item.input), item.expected, item.msg);
+        printIO(item, Utils.parseWildPreferenceQueryFromArgs);
+        t.deepEqual(Utils.parseWildPreferenceQueryFromArgs(item.input), item.expected);
     });
 
     cases.throws.forEach((item) => {
         const error = t.throws(() => {
-            parseQueryFromArgs(item.input);
+            Utils.parseWildPreferenceQueryFromArgs(item.input);
         }, Error);
+        printIE(item, error);
         t.truthy(item.expected.exec(error.message));
     });
 });
 
-// test('want.js:createWildPreferenceString', (t) => {
-//     t.
-// });
+function printIO(item, foo) {
+    console.log('\x1b[36m%s\x1b[0m', '++++++++++++++++++++++++++++++++++++++++');
+    console.log('\x1b[32m%s\x1b[0m', 'Input:');
+    console.log('\x1b[35m%s\x1b[0m', `   '${item.input}'`);
+    console.log('\x1b[32m%s\x1b[0m', 'Output:');
+    console.log('\x1b[35m%s\x1b[0m', foo(item.input));
+}
+
+function printIE(item, error) {
+    console.log('\x1b[36m%s\x1b[0m', '++++++++++++++++++++++++++++++++++++++++');
+    console.log('\x1b[32m%s\x1b[0m', 'Input:');
+    console.log('\x1b[35m%s\x1b[0m', `   '${item.input}'`);
+    console.log('\x1b[32m%s\x1b[0m', 'Output:');
+    console.log('\x1b[35m%s\x1b[0m', `   '${error.message.split('\n').join('\n   ')}'`);
+}
+
+// eslint-disable-next-line no-extend-native, func-names
+Object.prototype.toString = function () {
+    const keys = Object.keys(this);
+
+    let str = '   {';
+    keys.forEach((key) => {
+        str += `\n      ${key}: ${Array.isArray(this[key]) ? '[' : ''}${Array.isArray(this[key]) ? this[key].join(', ') : this[key]}${Array.isArray(this[key]) ? ']' : ''}`;
+    });
+    str += '\n   }';
+
+    return str;
+};
 
