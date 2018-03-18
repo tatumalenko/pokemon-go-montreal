aba316c3004da5cff6c6e2b486c43bd8601b2046
Tatum Alenko <tatum.alenko@gmail.com>
2018-03-18 00:29:12 -0400

    Refactor `createWildSpawnParams` and `sendEmbedToRecipients` into `Utils` class.

--- src/bots/pikachu/monitors/wildIncome.js
+++ src/bots/pikachu/monitors/wildIncome.js
@@ -12,7 +12,7 @@ module.exports = class {
 
     async run(msg, ...params) {
         try {
-            const spawn = new WildSpawn(createWildSpawnParams(msg));
+            const spawn = new WildSpawn(this.client.utils.createWildSpawnParams(msg));
 
             const spawnEmbed = this.createWildSpawnEmbed(spawn); // embed = {content, embed}, it's weird, I know..
 
@@ -24,9 +24,9 @@ module.exports = class {
 
             console.log(recipients.map(r => r.displayName));
 
-            // await sendEmbedToRepicients([this.client.guilds.get(this.client.configs.guildId).members.find('displayName', 'uphillsimplex')], spawnEmbed);
-            await sendEmbedToRepicients(recipients, spawnEmbed); // Post in DM channel to members
-            // await sendEmbedToRepicients([msg.guild.channels.find('name', 'wilds-post')], spawnEmbed); // Post in wilds-post channel
+            // await this.client.utils.sendEmbedToRepicients([this.client.guilds.get(this.client.configs.guildId).members.find('displayName', 'uphillsimplex')], spawnEmbed);
+            await this.client.utils.sendEmbedToRepicients(recipients, spawnEmbed); // Post in DM channel to members
+            // await this.client.utils.sendEmbedToRepicients([msg.guild.channels.find('name', 'wilds-post')], spawnEmbed); // Post in wilds-post channel
         } catch (e) {
             console.log(e);
         }
@@ -88,69 +88,3 @@ module.exports = class {
         }
     }
 };
-
-function createWildSpawnParams(message) {
-    const msg = message.content.replace(/\*/g, '');
-    const spawnParams = {
-        // Common to Spawn BELOW
-        name: /[\[.*\]]?\s?([a-zA-Z]+|[a-zA-Z]+ - \w|[a-zA-Z]+[\'][a-zA-Z]+)\s?-?\s? \((\d+%)\)/.exec(msg)
-            ? /[\[.*\]]?\s?([a-zA-Z]+|[a-zA-Z]+ - \w|[a-zA-Z]+[\'][a-zA-Z]+)\s?-?\s? \((\d+%)\)/.exec(msg)[1]
-            : /[\[.*\]]?\s?([a-zA-Z]+|[a-zA-Z]+\s?-?\s?\w)\s?-?\s?\n\n/.exec(msg)[1],
-        // Common to Spawn ABOVE
-        // --------------------------------
-        // Unique to WildSpawn BELOW
-        iv: /\((\d+)%\)/.exec(msg) ? Number(/\((\d+)%\)/.exec(msg)[1]) : 'NA',
-        cp: /\(CP: (\d+)\)/.exec(msg) ? Number(/\(CP: (\d+)\)/.exec(msg)[1]) : 'NA',
-        level: /\(Level: (-?\d+)\)/.exec(msg) ? Number(/\(Level: (-?\d+)\)/.exec(msg)[1]) : 'NA',
-        stats: {
-            iv: {
-                attack: /L30\+ IV: (\d+) - \d+ - \d+ .*/.exec(msg) ? Number(/L30\+ IV: (\d+) - \d+ - \d+ .*/.exec(msg)[1]) : 'NA',
-                defense: /L30\+ IV: \d+ - (\d+) - \d+ .*/.exec(msg) ? Number(/L30\+ IV: \d+ - (\d+) - \d+ .*/.exec(msg)[1]) : 'NA',
-                stamina: /L30\+ IV: \d+ - \d+ - (\d+) .*/.exec(msg) ? Number(/L30\+ IV: \d+ - \d+ - (\d+) .*/.exec(msg)[1]) : 'NA',
-            },
-        },
-        weatherBoosted: /Weather boosted: (.*)\n/.exec(msg) ? /Weather boosted: (.*)\n/.exec(msg)[1] : 'NA',
-        gender: /Gender: (.*)\n/.exec(msg) ? /Gender: (.*)\n/.exec(msg)[1] : 'NA',
-        // Unique to WildSpawn ABOVE
-        // --------------------------------
-        // Common to Spawn BELOW
-        moveset: {
-            fast: /L30\+ Moveset: (.*) - .*/.exec(msg) ? /L30\+ Moveset: (.*) - .*/.exec(msg)[1] : 'NA',
-            charge: /L30\+ Moveset: .* - (.*)/.exec(msg) ? /L30\+ Moveset: .* - (.*)/.exec(msg)[1] : 'NA',
-        },
-        time: {
-            remaining: /Until: .* \((.*) left\)/.exec(msg) ? /Until: .* \((.*) left\)/.exec(msg)[1] : 'NA',
-            ending: /Until: (.*) \(.* left\)/.exec(msg) ? /Until: (.*) \(.* left\)/.exec(msg)[1] : 'NA',
-        },
-        location: {
-            coordinates: {
-                latitude: /.*q=(\d+.\d+),-\d+.\d+/.exec(msg) ? Number(/.*q=(\d+.\d+),-\d+.\d+/.exec(msg)[1]) : 'NA',
-                longitude: /.*q=\d+.\d+,(-\d+.\d+)/.exec(msg) ? Number(/.*q=\d+.\d+,(-\d+.\d+)/.exec(msg)[1]) : 'NA',
-            },
-            address: /Address: (.*)\n/.exec(msg) ? /Address: (.*)\n/.exec(msg)[1].trim() : 'NA',
-            gmapsUrl: /Google Map: <?(.*)>?/.exec(msg) ? /Google Map: <?(.*)>?/.exec(msg)[1] : 'NA',
-        },
-    };
-    return spawnParams;
-}
-
-async function sendEmbedToRepicients(recipients, {
-    content,
-    embed,
-}) {
-    let totalAlertsSent = 0;
-    const successAlertRecipients = [];
-
-    await Promise.all(recipients.map(async (recipient) => {
-        try {
-            totalAlertsSent += 1;
-            successAlertRecipients.push(recipient.displayName !== undefined ? recipient.displayName : recipient.name);
-            return recipient.send(content, embed);
-        } catch (e) {
-            console.log(e);
-        }
-    }));
-
-    console.log(`${content}\nAlerts sent: ${totalAlertsSent} (${successAlertRecipients.join(', ')})`);
-    console.log('-----------------------------------------------------------------');
-}
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -229,6 +229,72 @@ class Utils {
             pokemonNames, neighbourhoodNames: neighbourhoodNames.sort(), ivs, levels,
         };
     }
+
+    static createWildSpawnParams(message) {
+        const msg = message.content.replace(/\*/g, '');
+        const spawnParams = {
+            // Common to Spawn BELOW
+            name: /[\[.*\]]?\s?([a-zA-Z]+|[a-zA-Z]+ - \w|[a-zA-Z]+[\'][a-zA-Z]+)\s?-?\s? \((\d+%)\)/.exec(msg)
+                ? /[\[.*\]]?\s?([a-zA-Z]+|[a-zA-Z]+ - \w|[a-zA-Z]+[\'][a-zA-Z]+)\s?-?\s? \((\d+%)\)/.exec(msg)[1]
+                : /[\[.*\]]?\s?([a-zA-Z]+|[a-zA-Z]+\s?-?\s?\w)\s?-?\s?\n\n/.exec(msg)[1],
+            // Common to Spawn ABOVE
+            // --------------------------------
+            // Unique to WildSpawn BELOW
+            iv: /\((\d+)%\)/.exec(msg) ? Number(/\((\d+)%\)/.exec(msg)[1]) : 'NA',
+            cp: /\(CP: (\d+)\)/.exec(msg) ? Number(/\(CP: (\d+)\)/.exec(msg)[1]) : 'NA',
+            level: /\(Level: (-?\d+)\)/.exec(msg) ? Number(/\(Level: (-?\d+)\)/.exec(msg)[1]) : 'NA',
+            stats: {
+                iv: {
+                    attack: /L30\+ IV: (\d+) - \d+ - \d+ .*/.exec(msg) ? Number(/L30\+ IV: (\d+) - \d+ - \d+ .*/.exec(msg)[1]) : 'NA',
+                    defense: /L30\+ IV: \d+ - (\d+) - \d+ .*/.exec(msg) ? Number(/L30\+ IV: \d+ - (\d+) - \d+ .*/.exec(msg)[1]) : 'NA',
+                    stamina: /L30\+ IV: \d+ - \d+ - (\d+) .*/.exec(msg) ? Number(/L30\+ IV: \d+ - \d+ - (\d+) .*/.exec(msg)[1]) : 'NA',
+                },
+            },
+            weatherBoosted: /Weather boosted: (.*)\n/.exec(msg) ? /Weather boosted: (.*)\n/.exec(msg)[1] : 'NA',
+            gender: /Gender: (.*)\n/.exec(msg) ? /Gender: (.*)\n/.exec(msg)[1] : 'NA',
+            // Unique to WildSpawn ABOVE
+            // --------------------------------
+            // Common to Spawn BELOW
+            moveset: {
+                fast: /L30\+ Moveset: (.*) - .*/.exec(msg) ? /L30\+ Moveset: (.*) - .*/.exec(msg)[1] : 'NA',
+                charge: /L30\+ Moveset: .* - (.*)/.exec(msg) ? /L30\+ Moveset: .* - (.*)/.exec(msg)[1] : 'NA',
+            },
+            time: {
+                remaining: /Until: .* \((.*) left\)/.exec(msg) ? /Until: .* \((.*) left\)/.exec(msg)[1] : 'NA',
+                ending: /Until: (.*) \(.* left\)/.exec(msg) ? /Until: (.*) \(.* left\)/.exec(msg)[1] : 'NA',
+            },
+            location: {
+                coordinates: {
+                    latitude: /.*q=(\d+.\d+),-\d+.\d+/.exec(msg) ? Number(/.*q=(\d+.\d+),-\d+.\d+/.exec(msg)[1]) : 'NA',
+                    longitude: /.*q=\d+.\d+,(-\d+.\d+)/.exec(msg) ? Number(/.*q=\d+.\d+,(-\d+.\d+)/.exec(msg)[1]) : 'NA',
+                },
+                address: /Address: (.*)\n/.exec(msg) ? /Address: (.*)\n/.exec(msg)[1].trim() : 'NA',
+                gmapsUrl: /Google Map: <?(.*)>?/.exec(msg) ? /Google Map: <?(.*)>?/.exec(msg)[1] : 'NA',
+            },
+        };
+        return spawnParams;
+    }
+
+    static async sendEmbedToRepicients(recipients, {
+        content,
+        embed,
+    }) {
+        let totalAlertsSent = 0;
+        const successAlertRecipients = [];
+
+        await Promise.all(recipients.map(async (recipient) => {
+            try {
+                totalAlertsSent += 1;
+                successAlertRecipients.push(recipient.displayName !== undefined ? recipient.displayName : recipient.name);
+                return recipient.send(content, embed);
+            } catch (e) {
+                console.log(e);
+            }
+        }));
+
+        console.log(`${content}\nAlerts sent: ${totalAlertsSent} (${successAlertRecipients.join(', ')})`);
+        console.log('-----------------------------------------------------------------');
+    }
 }
 
 module.exports = Utils;
