452ffd6e3198721687b3c9283cd8aa7caa3cf19d
Tatum Alenko <tatum.alenko@gmail.com>
2018-03-18 00:28:10 -0400

    Fix incorrect variable name usage.

--- src/bots/self/app.js
+++ src/bots/self/app.js
@@ -7,6 +7,7 @@ const WILD_IV0_IV90_CP2500_CHANNELS = ['iv0', 'iv90', 'cp2500'];
 const WILD_GEN1_CHANNELS = ['aerodactyl', 'alakazam', 'arcanine', 'blastoise', 'chansey', 'charizard', 'dragonair', 'dragonite', 'dratini', 'exeggutor', 'flareon', 'gengar', 'golem', 'gyarados', 'hitmonchan', 'hitmonlee', 'jolteon', 'lapras', 'lickitung', 'machamp', 'muk', 'porygon', 'rhydon', 'snorlax', 'vaporeon', 'venusaur'];
 const WILD_GEN2_CHANNELS = ['ampharos', 'blissey', 'donphan', 'feraligatr', 'flaaffy', 'forretress', 'hitmontop', 'larvitar', 'mareep', 'meganium', 'pupitar', 'togetic', 'typhlosion', 'tyranitar', 'unown'];
 const WILD_GEN3_CHANNELS = ['armaldo', 'bagon', 'beldum', 'chimecho', 'cradily', 'feebas', 'kirlia', 'lairon', 'metang', 'shelgon', 'slakoth', 'trapinch', 'vigoroth'];
+const WILD_NAMED_POKEMON_CHANNELS = [...WILD_GEN1_CHANNELS, ...WILD_GEN2_CHANNELS, ...WILD_GEN3_CHANNELS];
 
 const WILD_CHANNELS = [...WILD_IV0_IV90_CP2500_CHANNELS, ...WILD_GEN1_CHANNELS, ...WILD_GEN2_CHANNELS, ...WILD_GEN3_CHANNELS];
 const RAID_CHANNELS = ['raid-level-4', 'raid-legendary'];
@@ -39,13 +40,13 @@ client.on('message', (message) => {
 
 
             // Attempt to dead-end listener if finds spawn name to have a named channel after itself to prevent double DMs
-            if (IV90_CP2500_CHANNELS.some(c => c === message.channel.name.toLowerCase())) { // If this msg comes from iv90 or cp2500
+            if (WILD_IV0_IV90_CP2500_CHANNELS.some(c => c === message.channel.name.toLowerCase())) { // If this msg comes from iv90 or cp2500
                 const spawn = createSpawn(message); // Create spawn to extract name using name method used in Pikachu
 
                 console.log(`spawn.name.toLowerCase(): ${spawn.name.toLowerCase()}`);
                 console.log(`WILD_CHANNELS.some(c => c === spawn.name.toLowerCase()): ${WILD_CHANNELS.some(c => c === spawn.name.toLowerCase().replace('\'', ''))}`);
 
-                if (WILD_WITHOUT_GEN3_CHANNELS.some(c => c === spawn.name.toLowerCase().replace('\'', ''))) // If the spawn's name is in one of the wild channels
+                if (WILD_NAMED_POKEMON_CHANNELS.some(c => c === spawn.name.toLowerCase().replace('\'', ''))) // If the spawn's name is in one of the wild channels
                 { return; } // Dead-end listener knowing that another identical message will appear from the Pokemon's named channel
             }
 
