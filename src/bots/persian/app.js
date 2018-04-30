93b8bd113f4fc7f3c86762a327dcee28f6e9a02e
Emmanuel Chartrand <emman31@gmail.com>
2018-04-29 20:17:07 -0400

    Allow persian in secret-treehouse

--- src/bots/persian/app.js
+++ src/bots/persian/app.js
@@ -1,10 +1,7 @@
 const { Client } = require('../../models/Client');
 
-var client = new Client({
-    /*clientOptions: {
-        fetchAllMembers: true,
-    },*/
+const client = new Client({
     name: 'persian',
     appDirName: __dirname,
-    runIn: ['bot-testing', 'test-zone', 'dm'], // = ['all'] for any channel
+    runIn: ['bot-testing', 'test-zone', 'dm', 'secret-treehouse'], // = ['all'] for any channel
 }).login();
