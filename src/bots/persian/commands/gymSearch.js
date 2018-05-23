7e7a514d449944708f967de25bf949f2ccba11cb
Emmanuel Chartrand <emman31@gmail.com>
2018-05-23 16:23:04 -0400

    Added the ability to search gyms with the exact name

--- src/bots/persian/commands/gymSearch.js
+++ src/bots/persian/commands/gymSearch.js
@@ -21,27 +21,31 @@ module.exports = class {
                 return;
             }
 
-            // Build the Dictionary with individual words.
-            const gyms = await this.client.gymRepository.fetchAllGyms();
-            const gymDict = [];
-            gyms.forEach((element) => {
-                const split = element.name
-                    .replace('-', ' ')
-                    .replace('/', ' ')
-                    .split(' ');
-                gymDict.push(...split);
-            });
+            let query = args.join(' ');
+            if (!query.startsWith('"') || !query.endsWith('"')) {
+                // Build the Dictionary with individual words.
+                const gyms = await this.client.gymRepository.fetchAllGyms();
+                const gymDict = [];
+                gyms.forEach((element) => {
+                    const split = element.name
+                        .replace('-', ' ')
+                        .replace('/', ' ')
+                        .split(' ');
+                    gymDict.push(...split);
+                });
 
-            // Spellcheck!
-            const word = args.join(' ');
-            const corrections = [];
-            args.forEach((element) => {
-                const mostLikelyWord = this.client.spellchecker.getCorrections(element, gymDict)[0];
-                corrections.push(mostLikelyWord);
-            });
+                // Spellcheck!
+                const word = args.join(' ');
+                const corrections = [];
+                args.forEach((element) => {
+                    const mostLikelyWord = this.client.spellchecker.getCorrections(element, gymDict)[0];
+                    corrections.push(mostLikelyWord);
+                });
 
-            // Search!
-            const query = corrections.join(' ');
+                // Search!
+                query = corrections.join(' ');
+            }
+
             const foundGyms = await this.client.gymRepository.searchByName(query);
 
             // Output
