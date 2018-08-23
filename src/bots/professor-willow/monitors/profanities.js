68d93e726769d6a1626f71f7268b9f3fbf54b442
Tatum Alenko <tatum.alenko@gmail.com>
2018-08-23 17:04:55 -0400

    Decrease probability of replying.

--- src/bots/professor-willow/monitors/profanities.js
+++ src/bots/professor-willow/monitors/profanities.js
@@ -15,8 +15,8 @@ module.exports = class {
                 const cussWordsFrench = ['shit', 'damn', 'christ', 'jesus'];
                 const cussWordsEnglish = ['merde', 'calisse', 'tabarnak', 'esti'];
                 const randNumber = Math.random();
-                const shouldReply = randNumber > 0.1 && randNumber < 0.2;
-                if (cussWordsFrench.some(word => msg.content.toLowerCase().includes(word))) {
+                const shouldReply = randNumber > 0.1 && randNumber < 0.4;
+                if (shouldReply && cussWordsFrench.some(word => msg.content.toLowerCase().includes(word))) {
                     await msg.channel.send(cussWordsEnglish[Math.floor(Math.random() * cussWordsEnglish.length)]); 
                 } else if (cussWordsEnglish.some(word => msg.content.toLowerCase().includes(word))) {
                     await msg.channel.send(cussWordsFrench[Math.floor(Math.random() * cussWordsFrench.length)]); 
