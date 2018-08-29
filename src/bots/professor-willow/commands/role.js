d14427f626ed399c05378541b5ac4697f60661c0
Tatum Alenko <tatum.alenko@gmail.com>
2018-08-29 14:09:16 -0400

    Add `role` management command to PW.

---
+++ src/bots/professor-willow/commands/role.js
@@ -0,0 +1,45 @@
+module.exports = class {
+    constructor(...params) {
+        Object.assign(this, {
+            name: 'role',
+            enabled: true,
+            runIn: ['role-management'],
+            cooldown: 0,
+            aliases: ['roles'],
+            description: '',
+        });
+    }
+
+    async run(msg, { prefix, cmd, args }) {
+        try {
+            if (!msg.member.roles.some(role => role.name === 'admin' || role.name === 'mod' || role.name === 'mega-bot')) {
+                await msg.channel.send('You do not have permission for this command! You n\'avez pas la permissions d\'utiliser cette commande!');
+                return;
+            }
+            const roleToEditName = args[1];
+            const roleToEdit = await this.client.guilds.find('id', this.client.configs.guildId).roles.find('name', roleToEditName);
+            const roleMemberName = args[2];
+            const roleMember = msg.mentions.members.first(); // await this.client.guilds.find('id', this.client.configs.guildId).members.get(roleMemberName);
+
+            switch (args[0].toLowerCase()) {
+                case 'add':
+                    await roleMember.addRole(roleToEdit);
+                    await msg.channel.send(`Got it! Gave ${roleToEdit.name} access to ${roleMember.displayName}`);
+                    break;
+
+                case 'remove':
+                    await roleMember.removeRole(roleToEdit);
+                    await msg.channel.send(`Got it! Removed ${roleToEdit.name} access to ${roleMember.displayName}`);
+                    break;
+
+                default:
+                    break;
+            }
+
+            return;
+        } catch (e) {
+            console.error(`${process.env.name}.${this.name}: \n${e}`);
+            if (e.message) { await msg.guild.channels.find('name', this.client.configs.channels.botLogs).send(`${process.env.name}.${this.name}: ${e.message}`); }
+        }
+    }
+};
