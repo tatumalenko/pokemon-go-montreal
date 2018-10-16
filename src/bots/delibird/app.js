090faf7f54663ca2ebf95ada2bf7771568f1776b
Emmanuel Chartrand <emman31@gmail.com>
2018-10-15 21:47:04 -0400

    Delete old stuff

--- src/bots/delibird/app.js
+++
@@ -1,7 +0,0 @@
-const { Client } = require('../../models/Client');
-
-const client = new Client({
-    name: 'delibird',
-    appDirName: __dirname,
-    runIn: ['bot-testing', 'test-zone'], // = ['all'] for any channel
-}).login();
