8eb91ef7d2eb6d4a3edabe004bf0f224bb15648c
Emmanuel Chartrand <emman31@gmail.com>
2018-10-15 21:21:29 -0400

    Add pancakes to profanity, also... fuck

--- src/bots/professor-willow/monitors/profanities.js
+++ src/bots/professor-willow/monitors/profanities.js
@@ -11,21 +11,27 @@ module.exports = class {
     async run(msg, ...params) {
         try {
             // Only listen to messages not coming from the bot itself
-            if (this.client.configs['professor-willow'].clientId !== msg.member.id) {
-                const cussWordsFrench = ['shit', 'damn', 'christ', 'jesus'];
-                const cussWordsEnglish = ['merde', 'calisse', 'tabarnak', 'esti'];
-                const reactions = ['😱', '😤', '😭', '😝', '🥞'];
-                const randNumber = Math.random();
-                const shouldReply = randNumber > 0.1 && randNumber < 0.4;
-                if (!shouldReply) {
-                    // Do nothing..
-                } else if (cussWordsFrench.some(word => msg.content.toLowerCase().includes(word))) {
-                    await msg.react(reactions[Math.floor(Math.random() * cussWordsEnglish.length)]);
-                    await msg.channel.send(cussWordsEnglish[Math.floor(Math.random() * cussWordsEnglish.length)]);
-                } else if (cussWordsEnglish.some(word => msg.content.toLowerCase().includes(word))) {
-                    await msg.react(reactions[Math.floor(Math.random() * cussWordsEnglish.length)]);
-                    await msg.channel.send(cussWordsFrench[Math.floor(Math.random() * cussWordsFrench.length)]);
-                }
+
+            if (this.client.configs['professor-willow'].clientId === msg.member.id) {
+                return;
+            }
+
+            const randNumber = Math.random();
+            const shouldReply = randNumber > 0.1 && randNumber < 0.4;
+            if (!shouldReply) {
+                return;
+            }
+
+            const cussWordsFrench = ['shit', 'damn', 'christ', 'jesus', 'fuck'];
+            const cussWordsEnglish = ['merde', 'calisse', 'tabarnak', 'esti', 'putain'];
+            const reactions = ['😱', '😤', '😭', '😝', '🥞'];
+
+            if (cussWordsFrench.some(word => msg.content.toLowerCase().includes(word))) {
+                await msg.react(reactions[Math.floor(Math.random() * reactions.length)]);
+                await msg.channel.send(cussWordsEnglish[Math.floor(Math.random() * cussWordsEnglish.length)]);
+            } else if (cussWordsEnglish.some(word => msg.content.toLowerCase().includes(word))) {
+                await msg.react(reactions[Math.floor(Math.random() * reactions.length)]);
+                await msg.channel.send(cussWordsFrench[Math.floor(Math.random() * cussWordsFrench.length)]);
             }
         } catch (e) {
             console.error(e);
